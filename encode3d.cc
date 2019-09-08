096a547b245e9ab9fbb73c729c9209a81ab82c6c
Oisin Robinson <oisin.robinson@ichec.ie>
2019-09-08 21:34:31 +0100

    panic averted. was not making optimum use of A (int64_t) and B (uint64_t).  Now we have (a,b,c) distributed across (20+/-11,10+22) bits of (A,B).  Lost only 2,424 relations out of 31,274,442.

--- encode3d.cc
+++ encode3d.cc
@@ -31,11 +31,23 @@ int main (int argc, char** argv)
 		int64_t a = atoi(Astr.substr(0, Astr.find(separator2)).c_str());  Astr.erase(0, Astr.find(separator2) + 1);
 		int64_t b = atoi(Astr.substr(0, Astr.find(separator2)).c_str());  Astr.erase(0, Astr.find(separator2) + 1);
 		int64_t c = atoi(Astr.substr(0, Astr.find(separator2)).c_str());
-		
-		int64_t A = ((a+(1<<20))<<11) + ((b+(1<<20))>>11);
-		int64_t B = (b+(1<<20))%(1<<11)+((c+(1<<20))<<11);
 
-		cout << A << "," << B << ":" << line << endl << flush;
+        int ainfo = 0, binfo = 0, cinfo = 0;
+        int abits = a; while (abits>>=1) ainfo++;    // a is always supposed to be non-negative
+        int bbits = b; if (b < 0) { binfo++; bbits = -bbits; }; while (bbits>>=1) binfo++;
+        int cbits = c; if (c < 0) { cinfo++; cbits = -cbits; }; while (cbits>>=1) cinfo++;
+
+        if (ainfo <= 20 && binfo <= 22 && cinfo <= 22) {
+            uint64_t absb = b; if (b < 0) absb = -b;
+            int64_t A =  (a<<11) + (absb>>10);    // A = ((a+(1<<20))<<11) + ((b+(1<<20))>>11);
+            if (b < 0) A = -A;
+            uint64_t B = (absb%(1<<10)) + ((c+(1<<21))<<10); // B = (b+(1<<20))%(1<<11)+((c+(1<<20))<<11);
+
+            cout << A << "," << B << ":" << line << endl;
+        }
+        else {
+            cout << "#" << a << "," << b << "," << c << ":" << line << endl;
+        }
 	}
 
 	return 0;
