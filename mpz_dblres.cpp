c71150c7b591a9882de1ae081a4fb19a4d6e855a
Oisin Robinson <oisin.robinson@gmail.com>
2020-09-07 12:32:32 +0100

    adding double resultant code for future reference

---
+++ mpz_dblres.cpp
@@ -0,0 +1,30 @@
+#include <stack>
+
+
+using std::stack;
+using std::pair;
+using std::make_pair;
+
+int main(int argc, char** argv)
+{
+
+
+}
+
+
+
+void bivariate_resultant(mpz_poly* f, mpzpoly* g, mpzpoly out)
+{
+	// construct Sylvester matrix
+
+	
+	// compute determinant
+	stack< pair<int,int> > node;
+	stack<int> cols;
+	
+	node.push(make_pair(0,cols.push((1<<n)-1)));
+
+	while(!node.empty()) {
+			
+	}
+}
