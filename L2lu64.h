f468ec79b515ee43618b3b2a640d1d35f468a660
Oisin Robinson <oisin.robinson@gmail.com>
2020-09-16 14:00:50 +0100

    added 4d linear solver using Faddeev LeVerrier algorithm

--- L2lu64.h
+++ L2lu64.h
@@ -7,5 +7,6 @@ void int64L2(int64_t* b, int d);
 void printbasis(int64_t* b, int d, int pr, int w);
 void copysquarefloatarray(int64_t* src, int64_t* dest, int d);
 void luinv(int64_t* b, int64_t* M, int d);
+int64_t fadlev4d(int64_t* A, int64_t *M);
 
 #endif /* L2lu_h */
