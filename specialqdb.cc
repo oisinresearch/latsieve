b632e96726ddfed017cc5d189da6a93ec356e419
Oisin Robinson <oisin.robinson@gmail.com>
2022-02-19 21:20:53 +0000

    make specialqdb faster by storing in memory, preparing statements.

--- specialqdb.cc
+++ specialqdb.cc
@@ -45,8 +45,9 @@ int main(int argc, char** argv)
 	int64_t q0 = 1;
 	int64_t q1 = 1;
 
-    sqlite3* pDb = NULL;
-    sqlite3_stmt* query = NULL;
+    sqlite3* db = NULL;
+    sqlite3_stmt* query1 = NULL;
+    sqlite3_stmt* query2 = NULL;
     int ret = 0;
 	string sql = "";
 	// initialize engine
@@ -56,7 +57,7 @@ int main(int argc, char** argv)
 	}
 
 	// open connection to a DB
-	if (SQLITE_OK != (ret = sqlite3_open_v2(dbname.c_str(), &pDb,
+	if (SQLITE_OK != (ret = sqlite3_open_v2(dbname.c_str(), &db,
 		SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL))) {
 		printf("Failed to open conn: %d\n", ret);
 		goto done;
@@ -64,71 +65,83 @@ int main(int argc, char** argv)
 
 	// prepare the statement
 	sql = "CREATE TABLE specialq0 (q INT NOT NULL, count INT, PRIMARY KEY (q));";
-	if (SQLITE_OK != (ret = sqlite3_prepare_v2(pDb, sql.c_str(), -1, &query, NULL))) {
-		printf("Failed to prepare create table: %d, %s\n", ret, sqlite3_errmsg(pDb));
+	if (SQLITE_OK != (ret = sqlite3_prepare_v2(db, sql.c_str(), -1, &query1, NULL))) {
+		printf("Failed to prepare create table: %d, %s\n", ret, sqlite3_errmsg(db));
 		goto done;
 	}
 
 	// step to 1st row of data
-	if (SQLITE_DONE != (ret = sqlite3_step(query))) {
-		printf("Failed to create table: %d, %s\n", ret, sqlite3_errmsg(pDb));
+	if (SQLITE_DONE != (ret = sqlite3_step(query1))) {
+		printf("Failed to create table: %d, %s\n", ret, sqlite3_errmsg(db));
 		goto done;
 	}
 
 	// prepare the statement
 	sql = "CREATE TABLE specialq1 (q INT NOT NULL, count INT, PRIMARY KEY (q));";
-	if (SQLITE_OK != (ret = sqlite3_prepare_v2(pDb, sql.c_str(), -1, &query, NULL)))
+	if (SQLITE_OK != (ret = sqlite3_prepare_v2(db, sql.c_str(), -1, &query2, NULL)))
 	{
-		printf("Failed to prepare create table: %d, %s\n", ret, sqlite3_errmsg(pDb));
+		printf("Failed to prepare create table: %d, %s\n", ret, sqlite3_errmsg(db));
 		goto done;
 	}
 
 	// step to 1st row of data
-	if (SQLITE_DONE != (ret = sqlite3_step(query))) {
-		printf("Failed to create table: %d, %s\n", ret, sqlite3_errmsg(pDb));
+	if (SQLITE_DONE != (ret = sqlite3_step(query2))) {
+		printf("Failed to create table: %d, %s\n", ret, sqlite3_errmsg(db));
 		goto done;
 	}
+
+	// prepare the statement
+	sql = "INSERT INTO specialq0 (q, count) VALUES (?, NULL);";
+	if (SQLITE_OK != (ret = sqlite3_prepare_v2(db, sql.c_str(), -1, &query1, NULL))) {
+		printf("Failed to prepare insert: %d, %s\n", ret, sqlite3_errmsg(db));
+		goto done;
+	}
+
+	// prepare the statement
+	sql = "INSERT INTO specialq1 (q, count) VALUES (?, NULL);";
+	if (SQLITE_OK != (ret = sqlite3_prepare_v2(db, sql.c_str(), -1, &query2, NULL))) {
+		printf("Failed to prepare insert: %d, %s\n", ret, sqlite3_errmsg(db));
+		goto done;
+	}
+
+	// store in memory
+	sqlite3_exec(db, "PRAGMA journal_mode = MEMORY", NULL, NULL, NULL);
 
 	while (q0 < q0max || q1 < q1max) {
 		mpz_set_ui(qmpz0, q0);
 		mpz_nextprime(qmpz0, qmpz0);
 		q0 = mpz_get_ui(qmpz0);
 		if (q0 < q0max) {
-			// prepare the statement
-			sql = "INSERT INTO specialq0 (q, count) VALUES (" + to_string(q0) + ", 0);";
-			if (SQLITE_OK != (ret = sqlite3_prepare_v2(pDb, sql.c_str(), -1, &query, NULL))) {
-				printf("Failed to prepare insert: %d, %s\n", ret, sqlite3_errmsg(pDb));
-				goto done;
-			}
+			sqlite3_bind_int(query1, 1, q0);
 			// execute query
-			if (SQLITE_DONE != (ret = sqlite3_step(query))) {
-				printf("Failed to insert: %d, %s\n", ret, sqlite3_errmsg(pDb));
+			if (SQLITE_DONE != (ret = sqlite3_step(query1))) {
+				printf("Failed to insert: %d, %s\n", ret, sqlite3_errmsg(db));
 				goto done;
 			}
+			sqlite3_clear_bindings(query1);
+			sqlite3_reset(query1);
 		}
 		mpz_set_ui(qmpz1, q1);
 		mpz_nextprime(qmpz1, qmpz1);
 		q1 = mpz_get_ui(qmpz1);
 		if (q1 < q1max) {
-			// prepare the statement
-			sql = "INSERT INTO specialq1 (q, count) VALUES (" + to_string(q1) + ", 0);";
-			if (SQLITE_OK != (ret = sqlite3_prepare_v2(pDb, sql.c_str(), -1, &query, NULL))) {
-				printf("Failed to prepare insert: %d, %s\n", ret, sqlite3_errmsg(pDb));
-				goto done;
-			}
+			sqlite3_bind_int(query2, 1, q1);
 			// execute query
-			if (SQLITE_DONE != (ret = sqlite3_step(query))) {
-				printf("Failed to insert: %d, %s\n", ret, sqlite3_errmsg(pDb));
+			if (SQLITE_DONE != (ret = sqlite3_step(query2))) {
+				printf("Failed to insert: %d, %s\n", ret, sqlite3_errmsg(db));
 				goto done;
 			}
+			sqlite3_clear_bindings(query2);
+			sqlite3_reset(query2);
 		}
 	}
 
 	done:
 
     // cleanup
-    if (NULL != query) sqlite3_finalize(query);
-    if (NULL != pDb) sqlite3_close(pDb);
+    if (NULL != query1) sqlite3_finalize(query1);
+    if (NULL != query2) sqlite3_finalize(query2);
+    if (NULL != db) sqlite3_close(db);
     sqlite3_shutdown();
 	mpz_clear(qmpz0);
 	mpz_clear(qmpz1);
